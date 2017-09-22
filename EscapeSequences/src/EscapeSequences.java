a1380e11b1f49bb0c3be9d25e1549f285cf79299
mbuckley2018 <mbuckley2018@cchsraider.net>
2017-09-22 10:07:49 -0400

    initial commit

---
+++ EscapeSequences/src/EscapeSequences.java
@@ -0,0 +1,10 @@
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("\"Skiing combines outdoor fun with knocking down trees with your face.\"");
+System.out.println("a\\b\\c\\d");
+	}
+
+}
